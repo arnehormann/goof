708b8109cf5a7f4dd2e34cc8aa100e3b6ccc4c44
Arne Hormann <arnehormann@gmail.com>
2014-02-01 11:33:23 +0100

    some new benchmarks... less promising, sadly

--- upto_test.go
+++ upto_test.go
@@ -2,20 +2,58 @@ package goof
 
 import "testing"
 
+const innerLoops = 10000
+
+func BenchmarkFor(b *testing.B) {
+	n := 0
+	for i := 0; i < b.N; i++ {
+		n = i
+	}
+	var _ = n
+}
+
 func BenchmarkUpTo(b *testing.B) {
 	n := 0
-	b.ResetTimer()
 	for i := range UpTo(b.N) {
 		n = i
 	}
 	var _ = n
 }
 
-func BenchmarkFor(b *testing.B) {
-	n := 0
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		n = i
+func BenchmarkUnusedLoopVarUpTo(b *testing.B) {
+	for _ = range UpTo(b.N) {
 	}
-	var _ = n
+}
+
+func BenchmarkUnusedLoopVarFor(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+	}
+}
+
+func BenchmarkDoubleFor(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for j := 0; j < innerLoops; j++ {
+		}
+	}
+}
+
+func BenchmarkDoubleUpTo(b *testing.B) {
+	for _ = range UpTo(b.N) {
+		for _ = range UpTo(innerLoops) {
+		}
+	}
+}
+
+func BenchmarkFor_UpTo(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _ = range UpTo(innerLoops) {
+		}
+	}
+}
+
+func BenchmarkUpTo_For(b *testing.B) {
+	for _ = range UpTo(b.N) {
+		for j := 0; j < innerLoops; j++ {
+		}
+	}
 }
