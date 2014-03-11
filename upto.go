75b9addc492c49bb41bee2a514e2433905c83cdc
Arne Hormann <arnehormann@users.noreply.github.com>
2014-03-11 05:30:43 +0100

    Update upto.go

--- upto.go
+++ upto.go
@@ -3,6 +3,6 @@ package goof
 // UpTo returns a slice taking up no memory for use with
 // for i := range UpTo(n) { ... }
 func UpTo(i int) []struct{} {
-	upto := [(1 << 31) - 1]struct{}{}
+	upto := [^uint(0) >> 1]struct{}{}
 	return upto[:i]
 }
