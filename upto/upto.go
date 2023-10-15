62382400b54dc2dfe6a516fd43e284bfa74eee07
Arne Hormann <arnehormann@gmail.com>
2023-10-15 14:15:17 +0200

    drop upto

--- upto/upto.go
+++
@@ -1,8 +0,0 @@
-package upto
-
-// UpTo returns a slice taking up no memory for use with
-// for i := range UpTo(n) { ... }
-func UpTo(i int) []struct{} {
-	upto := [i]struct{}{}
-	return upto[:]
-}
